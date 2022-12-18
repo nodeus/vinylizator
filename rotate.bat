f98c6d94787c275d722655eafb6dc6d645c63f2b
nodeus <nodeus@yandex.ru>
2022-12-18 13:46:32 +0500

    Extract png from mp3

--- rotate.bat
+++ rotate.bat
@@ -1,6 +1,6 @@
 rem ffmpeg -y -i rot.png -t 10 out.mp4
 rem extract albumart
-ffmpeg -i audio.mp3 -filter:v scale=-2:500 -an gfx/cover.png
+ffmpeg -y -i audio.mp3 -c:v copy -an gfx/cover.png
 rem SET time=15
 rem ffmpeg -framerate 60 -i rot.png -i audio.mp3 -c:v libx265 -x265-params lossless=1 -loop 1 -pix_fmt yuv420p -movflags faststart out.mp4
 rem ffmpeg -y -i out.mp4 -vf "rotate=2*PI*t/%time%" -c:a copy out2.mp4
