1a5064568ab160316f4f4390a6524b9be58e2f36
nodeus <nodeus@yandex.ru>
2022-12-18 14:11:19 +0500

    ffmpeg generate video with cover image and music duration
    + add *.mp4 to .gitignore

--- rotate.bat
+++ rotate.bat
@@ -1,8 +1,21 @@
+rem set time in sec for 360 degree rotate
+
+set time=60
+
 rem ffmpeg -y -i rot.png -t 10 out.mp4
+
 rem extract albumart
+
 ffmpeg -y -i audio.mp3 -c:v copy -an gfx/cover.png
-rem SET time=15
-rem ffmpeg -framerate 60 -i rot.png -i audio.mp3 -c:v libx265 -x265-params lossless=1 -loop 1 -pix_fmt yuv420p -movflags faststart out.mp4
+
+rem generate cover from metadata
+
+rem generate video cover from albumart with music duration
+
+ffmpeg -loop 1 -framerate 60 -i gfx/cover.png -i audio.mp3 -c:v libx265 -x265-params lossless=1 -shortest -pix_fmt yuv420p -movflags faststart video/out.mp4
+
+rem generate rotated cover from albumart
+
 rem ffmpeg -y -i out.mp4 -vf "rotate=2*PI*t/%time%" -c:a copy out2.mp4
-pause
+
 rem -filter rotate=2*pi+5/T -vf "scale=500:500,loop=-1:1"
