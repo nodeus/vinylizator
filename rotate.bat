efaee0e79dfa8a07fb9a05ec7b1b336502a7ba77
nodeus <nodeus@yandex.ru>
2022-12-18 20:16:19 +0500

    add scaling to cover image
    albumart image extract and then scaled to cover image

--- rotate.bat
+++ rotate.bat
@@ -1,23 +1,29 @@
+rem using ffmpeg https://ffmpeg.org/
+rem using graphicsmagic http://www.graphicsmagick.org
+
 @cls
-@echo off
+rem @echo off
 rem set time in sec for 360 degree rotate
 
-@set time=45
-
-rem ffmpeg -y -i rot.png -t 10 out.mp4
+@set time=15
 
 rem extract albumart
+@echo extract albumart
+ffmpeg -loglevel error -y -i audio.mp3 -c:v copy -an gfx/albumart.png
 
-@ffmpeg -y -i audio.mp3 -c:v copy -an gfx/cover.png
+rem resize image
+
+ffmpeg -loglevel error -y -i gfx/albumart.png -filter_complex scale=-2:368 -sws_flags spline gfx/cover.png
 
 rem generate cover from metadata
 
 rem generate video cover from albumart with music duration
-
-@ffmpeg -y -loop 1 -framerate 60 -i gfx/cover.png -i audio.mp3 -c:v libx265 -x265-params lossless=1 -shortest -pix_fmt yuv420p -movflags faststart video/out.mp4
+@echo generate video cover
+ffmpeg -loglevel error -y -loop 1 -framerate 60 -i gfx/cover.png -i audio.mp3 -c:v libx265 -x265-params lossless=1 -shortest -pix_fmt yuv420p -movflags faststart video/out.mp4
 
 rem generate rotated cover from albumart
+@echo generate rotated video
+ffmpeg -loglevel error -y -i video/out.mp4  -vf "rotate=2*PI*t/%time%" -c:a copy video/out-rotated.mp4
 
-@ffmpeg -y -i video/out.mp4 -c:v libx265 -x265-params lossless=1 -pix_fmt yuv420p -movflags faststart -vf "rotate=2*PI*t/%time%" -c:a copy video/out-rotated.mp4
-
-rem -filter rotate=2*pi+5/T -vf "scale=500:500,loop=-1:1"
+rem -filter rotate=2*pi+5/T -vf "scale=500:500,loop=-1:1" lossless=1
+rem -c:v libx265 -x265-params lossless=1 -shortest -pix_fmt yuv420p -movflags faststart
